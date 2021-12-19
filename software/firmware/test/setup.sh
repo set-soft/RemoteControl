197b0a1ca33bca62e4c10fd573bd6575b1752f87
erichstuder <erichstuder@optiplex.home>
2021-12-19 12:32:09 +0100

    add cpputest code completion

--- software/firmware/test/setup.sh
+++ software/firmware/test/setup.sh
@@ -1,1 +1,7 @@
 sudo apt install docker docker-compose
+
+# Add cpputest folder for code completion (see: platformio.ini).
+FileName=cpputest-4.0.tar.gz
+wget https://github.com/cpputest/cpputest/releases/download/v4.0/$FileName
+tar -xf $FileName
+rm $FileName
